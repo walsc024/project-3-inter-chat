7c80a416f79f51ec1b37ea2f962fd1c14f9c9490
David Shonn <davidlshonn@icloud.com>
2021-04-30 16:29:00 +0100

    placeholder converted into a component

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import SignUpForm from "../components/SignUpForm";
 import LoginButton from "../components/LoginButton";
 import Dashboard from "./DashBoard";
 import SignUpButton from "../components/SignUpButton";
